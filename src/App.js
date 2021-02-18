8078ce0d6f8fba1e2fd43569b69ac20ee9f622c8
denvermullets <devmullets@gmail.com>
2021-02-18 07:50:43 -0500

    :recycle: add user auth-amplify, add storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 
-import { API } from 'aws-amplify'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { API, Auth } from 'aws-amplify'
 import { listPosts } from './graphql/queries'
 
 function App() {
-  return <></>
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetchPosts()
+    checkUser()
+    return () => {}
+  }, [])
+
+  async function checkUser() {
+    const user = await Auth.currentAuthenticatedUser()
+    console.log('user: ', user)
+    console.log('user attributes: ', user.attributes)
+  }
+
+  async function fetchPosts() {
+    try {
+      const postData = await API.graphql({ query: listPosts })
+      setPosts(postData.data.listPosts.items)
+    } catch (err) {
+      console.log({ err })
+    }
+  }
+
+  return (
+    <div>
+      <AmplifySignOut />
+      <h1>Hello World</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h3>{post.name}</h3>
+          <p>{post.location}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-export default App
+export default withAuthenticator(App)
