7921f63a4738f427c975ad1c5df8271ce7706a94
denvermullets <devmullets@gmail.com>
2021-02-18 08:36:55 -0500

    :construction: updated to add filepicker

--- src/App.js
+++ src/App.js
@@ -1,44 +1,64 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 
+import { Storage } from 'aws-amplify'
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import { API, Auth } from 'aws-amplify'
 import { listPosts } from './graphql/queries'
+import { v4 as uuid } from 'uuid'
 
 function App() {
-  const [posts, setPosts] = useState([])
+  // const [posts, setPosts] = useState([])
+  const [images, setImages] = useState([])
 
   useEffect(() => {
-    fetchPosts()
-    checkUser()
+    // fetchPosts()
+    // checkUser()
+    fetchImages()
     return () => {}
   }, [])
 
-  async function checkUser() {
-    const user = await Auth.currentAuthenticatedUser()
-    console.log('user: ', user)
-    console.log('user attributes: ', user.attributes)
+  async function fetchImages() {
+    let s3images = await Storage.list('')
+    s3images = await Promise.all(
+      s3images.map(async (image) => {
+        const signedImage = await Storage.get(image.key)
+        return signedImage
+      })
+    )
   }
 
-  async function fetchPosts() {
-    try {
-      const postData = await API.graphql({ query: listPosts })
-      setPosts(postData.data.listPosts.items)
-    } catch (err) {
-      console.log({ err })
-    }
+  function onChange(e) {
+    if (!e.target.files[0]) return
+    const file = e.target.files[0]
+    Storage.put(uuid(), file).then(() => fetchImages())
   }
+  // async function checkUser() {
+  //   const user = await Auth.currentAuthenticatedUser()
+  //   console.log('user: ', user)
+  //   console.log('user attributes: ', user.attributes)
+  // }
+
+  // async function fetchPosts() {
+  //   try {
+  //     const postData = await API.graphql({ query: listPosts })
+  //     setPosts(postData.data.listPosts.items)
+  //   } catch (err) {
+  //     console.log({ err })
+  //   }
+  // }
 
   return (
     <div>
+      <h1>Photo Album</h1>
+      <span>Add new image</span>
+      <input type='file' accept='image/png' onChange={onChange} />
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {images.map((image) => (
+          <img src={image} style={{ width: 400, marginBottom: 10 }} />
+        ))}
+      </div>
       <AmplifySignOut />
-      <h1>Hello World</h1>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.name}</h3>
-          <p>{post.location}</p>
-        </div>
-      ))}
     </div>
   )
 }
